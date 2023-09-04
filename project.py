c8339cb278cdf08b9ce33d03c547823efda21154
tirzasam07 <83017742+tirzasam07@users.noreply.github.com>
2023-09-04 18:33:55 +0200

    simple calculator

--- project.py
+++ project.py
@@ -1,5 +1,18 @@
 # Ask user for their name 
-name = input("What is your name ? ")
+name = input("What is your name ? ").strip().title()
+
+#remove whitespace from str
+#name = name.strip()
+
+#capitalize user's name
+#name = name.capitalize()
+
+#name = name.strip().title()
+
+#ask the user user first and last name then split
+first, last = name.split(" ")
 
 #Say hello to the user
-print(f"Hello, {name}")
+print(f"Hello, {first}")
+
+
